a80e003bdc661f1fe421c394dfe2faefeb7c7e61
Ryan Paton <rpaton333@gmail.com>
2020-07-27 10:26:18 +1200

    Added card images and JS file

---
+++ legion.js
@@ -0,0 +1,14 @@
+/*
+legion.js
+
+Web application for the setup of battle cards for the skirmish format
+in the Star Wars: Legion tabletop miniatures game.
+
+Ryan Paton
+2020-07-27
+*/
+
+function shuffleCards() {
+	// Shuffles the battle cards and displays them in a 3 by 3 grid
+	
+}
