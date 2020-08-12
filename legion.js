567f10b72465b8a5e1bfee8c11891a5a0a50b317
Ryan Paton <rpaton333@gmail.com>
2020-08-12 18:28:02 +1200

    The defined battlefield cards display after all eliminations/passes have been made

--- legion.js
+++ legion.js
@@ -24,200 +24,248 @@ const IMAGE_STYLE = "width:100%;max-width:300px";
 const P_BLUE = "Blue Player";
 const P_RED = "Red Player";
 
-var setupStatus = {currentPlayer:P_BLUE, blueTurns:0, redTurns:0};
+var setupStatus = {currentPlayer:P_BLUE, redTurns:0, ready:true};
 
 var deploymentDeck = [{name:"Battle Lines", source:"card_images/deployment_battle_lines.png"},
-	{name:"Faceoff", source:"card_images/deployment_faceoff.png"},
-	{name:"Flanking Positions", source:"card_images/deployment_flanking.png"},
-	{name:"Meeting Engagement", source:"card_images/deployment_meeting.png"}];
+    {name:"Faceoff", source:"card_images/deployment_faceoff.png"},
+    {name:"Flanking Positions", source:"card_images/deployment_flanking.png"},
+    {name:"Meeting Engagement", source:"card_images/deployment_meeting.png"}];
 
 var objectiveDeck = [{name:"Breach", source:"card_images/objective_breach.png"},
-	{name:"Control", source:"card_images/objective_control.png"},
-	{name:"Elimination", source:"card_images/objective_elimination.png"},
-	{name:"Pivotal Positions", source:"card_images/objective_positions.png"}];
+    {name:"Control", source:"card_images/objective_control.png"},
+    {name:"Elimination", source:"card_images/objective_elimination.png"},
+    {name:"Pivotal Positions", source:"card_images/objective_positions.png"}];
 
 var conditionDeck = [{name:"Clear Conditions", source:"card_images/condition_clear.png"},
-	{name:"Dawn", source:"card_images/condition_dawn.png"},
-	{name:"Improvised Defenses", source:"card_images/condition_defenses.png"},
-	{name:"War Weary", source:"card_images/condition_war_weary.png"}];
-	
+    {name:"Dawn", source:"card_images/condition_dawn.png"},
+    {name:"Improvised Defenses", source:"card_images/condition_defenses.png"},
+    {name:"War Weary", source:"card_images/condition_war_weary.png"}];
+
 function resetSetupStatus() {
-	setupStatus.currentPlayer = P_BLUE;
-	setupStatus.blueTurns = 0;
-	setupStatus.redTurns = 0;
+    setupStatus.currentPlayer = P_BLUE;
+    setupStatus.redTurns = 0;
+    setupStatus.ready = true;
 }
 
 function resetCardOpacity() {
-	var decks = [deploymentDeck, objectiveDeck, conditionDeck];
-	var i, j;
-	
-	for (i = 0; i < decks.length; i++) {
-		var currentDeck = decks[i];
-		for (j = 0; j < currentDeck.length; j++) {
-			currentDeck[j].html.firstChild.className = CARD_IMAGE;
-		}
-	}
+    var decks = [deploymentDeck, objectiveDeck, conditionDeck];
+    var i, j;
+    
+    for (i = 0; i < decks.length; i++) {
+        var currentDeck = decks[i];
+        for (j = 0; j < currentDeck.length; j++) {
+            currentDeck[j].html.firstChild.className = CARD_IMAGE;
+        }
+    }
 }
 
 function changePlayer() {
-	// Changes current player 
-	if (setupStatus.currentPlayer == P_BLUE) {
-		setupStatus.currentPlayer = P_RED;
-		setupStatus.blueTurns += 1;
-	} else {
-		setupStatus.currentPlayer = P_BLUE;
-		setupStatus.redTurns += 1;
-	}
-	
-	if (setupStatus.redTurns < 2) {
-		if (setupStatus.currentPlayer == P_BLUE) {
-			displayMessage(MSG_BLUE_TURN);
-		} else {
-			displayMessage(MSG_RED_TURN);
-		}
-	}
-	else {
-		displayMessage("Battle cards are set");
-	}
+    // Changes current player
+    if (setupStatus.currentPlayer == P_BLUE) {
+        setupStatus.currentPlayer = P_RED;
+    } else {
+        setupStatus.currentPlayer = P_BLUE;
+        setupStatus.redTurns += 1;
+    }
 }
 
 function displayMessage(message) {
-	// Displays a message panel
-	var messagePanel = document.getElementById("messagePanel");
-	messagePanel.innerHTML = message;
-	messagePanel.parentNode.style.display = "block";
+    // Displays a message panel
+    var messagePanel = document.getElementById("messagePanel");
+    messagePanel.innerHTML = message;
+    messagePanel.parentNode.style.display = "block";
 }
 
 function isValidCardChoice(cardElement) {
-	// Returns true if the selected card is a valid option
-	// i.e. is the leftmost non eliminated card and is not the rightmost card
-	// TODO: this may not work on older browsers, fix if neccessary
-	if (cardElement.classList.contains("w3-opacity-max")) {
-		return false;
-	}
-	
-	var cardDiv = cardElement.parentNode
-	var rowDiv = cardDiv.parentNode;
-	var index = Array.prototype.indexOf.call(rowDiv.children, cardDiv);
-	var result = true;
-	
-	// Check if card is the rightmost one
-	if (index == 2) {
-		return false
-	}
-	
-	// Check if card is the leftmost one
-	if (index != 0) {
-		var i = index - 1;
-		
-		while (i >= 0) {
-			if (!rowDiv.children[i].firstChild.classList.contains("w3-opacity-max")) {
-				result = false
-			}
-			i--;
-		}
-	}
-	return result;
+    // Returns true if the selected card is a valid option
+    // i.e. is the leftmost non eliminated card and is not the rightmost card
+    // TODO: this may not work on older browsers, fix if neccessary
+    if (cardElement.classList.contains("w3-opacity-max")) {
+        return false;
+    }
+    
+    var cardDiv = cardElement.parentNode
+    var rowDiv = cardDiv.parentNode;
+    var index = Array.prototype.indexOf.call(rowDiv.children, cardDiv);
+    var result = true;
+    
+    // Check if card is the rightmost one
+    if (index == 2) {
+        return false
+    }
+    
+    // Check if card is the leftmost one
+    if (index != 0) {
+        var i = index - 1;
+        
+        while (i >= 0) {
+            if (!rowDiv.children[i].firstChild.classList.contains("w3-opacity-max")) {
+                result = false
+            }
+            i--;
+        }
+    }
+    return result;
+}
+
+function updateBattleStatus() {
+    // Displays info related to the selection process then checks if it is
+    // finished and updates the setup staus
+    if (setupStatus.redTurns < 2) {
+        if (setupStatus.currentPlayer == P_BLUE) {
+            displayMessage(MSG_BLUE_TURN);
+        } else {
+            displayMessage(MSG_RED_TURN);
+        }
+    }
+    else {
+        setupStatus.ready = false;
+        displayMessage("Battle cards are set");
+        hidePassButton();
+        displaySelectedBattleCards();
+    }
 }
 
 function cardClicked(cardElement) {
-	// Handles a card click
-	
-	if (isValidCardChoice(cardElement) && (setupStatus.redTurns < 2)) {
-		cardElement.className += " w3-opacity-max";
-		changePlayer();
-	}
+    // Handles a card click
+    
+    if (setupStatus.ready) {
+        if (isValidCardChoice(cardElement)) {
+            cardElement.className += " w3-opacity-max";
+            changePlayer();
+            updateBattleStatus();
+        }
+    }
+}
+
+function getLeftmostCard(deck) {
+    // Returns the leftmost card that wasn't eliminated
+    var i = 0;
+    var found = false;
+    var result = deck[2];
+    
+    while ((!found) && i < 2) {
+        if (!(deck[i].html.firstChild.classList.contains("w3-opacity-max"))) {
+            result = deck[i];
+            found = true;
+        }
+        i++;
+    }
+    return result;
 }
 
 function shuffleDeck(deck) {
-	// Shuffle deck using Fisher-Yates algorithm
-	
-	var currentIndex = deck.length;
-	var tempValue, i;
-	
-	while (currentIndex > 1) {
-		// Pick a random unshuffled value
-		i = Math.floor(Math.random() * currentIndex)
-		currentIndex--;
-		
-		// Swap the value with the current one
-		// starting from the end
-		tempValue = deck[currentIndex];
-		deck[currentIndex] = deck[i];
-		deck[i] = tempValue;
-	}
+    // Shuffle deck using Fisher-Yates algorithm
+    
+    var currentIndex = deck.length;
+    var tempValue, i;
+    
+    while (currentIndex > 1) {
+        // Pick a random unshuffled value
+        i = Math.floor(Math.random() * currentIndex)
+        currentIndex--;
+        
+        // Swap the value with the current one
+        // starting from the end
+        tempValue = deck[currentIndex];
+        deck[currentIndex] = deck[i];
+        deck[i] = tempValue;
+    }
 }
 
 function shuffleCards() {
-	// Shuffles the battle cards and displays them in a 3 by 3 grid
-	shuffleDeck(deploymentDeck);
-	shuffleDeck(objectiveDeck);
-	shuffleDeck(conditionDeck);
+    // Shuffles the battle cards and displays them in a 3 by 3 grid
+    shuffleDeck(deploymentDeck);
+    shuffleDeck(objectiveDeck);
+    shuffleDeck(conditionDeck);
 }
 
 function generateCardHTML(card) {
-	// Generates HTML for displaying a single card
-	var cardDiv = document.createElement("div");
-	var cardImage = document.createElement("img");
-	
-	cardDiv.className = DIV_THIRD;
-	cardImage.className = CARD_IMAGE;
-	cardImage.style = IMAGE_STYLE;
-	cardImage.src = card.source;
-	cardImage.alt = card.name;
-	cardImage.onclick = function(){cardClicked(this)};
-	
-	cardDiv.appendChild(cardImage);
-	card.html = cardDiv;
+    // Generates HTML for displaying a single card
+    var cardDiv = document.createElement("div");
+    var cardImage = document.createElement("img");
+    
+    cardDiv.className = DIV_THIRD;
+    cardImage.className = CARD_IMAGE;
+    cardImage.style = IMAGE_STYLE;
+    cardImage.src = card.source;
+    cardImage.alt = card.name;
+    cardImage.onclick = function(){cardClicked(this)};
+    
+    cardDiv.appendChild(cardImage);
+    card.html = cardDiv;
 }
 
 function generateDeckHTML(deck) {
-	// Generates HTML for displaying the deck
-	var i;
-	for (i = 0; i < deck.length; i++) {
-		generateCardHTML(deck[i]);
-	}
+    // Generates HTML for displaying the deck
+    var i;
+    for (i = 0; i < deck.length; i++) {
+        generateCardHTML(deck[i]);
+    }
 }
 
 function displayDeck(displayElement, deck) {
-	// Displays the top three battle cards ready for selection
-	var i, numCards = 3;
-	displayElement.innerHTML = "";
-	
-	for (i = 0; i < numCards; i++) {
-		displayElement.appendChild(deck[i].html);
-	}
+    // Displays the top three battle cards ready for selection
+    var i, numCards = 3;
+    displayElement.innerHTML = "";
+    
+    for (i = 0; i < numCards; i++) {
+        displayElement.appendChild(deck[i].html);
+    }
 }
 
 function displayBattleCards() {
-	// Displays the top three battle cards from each deck ready for selection
-	displayDeck(document.getElementById("deployments"), deploymentDeck);
-	displayDeck(document.getElementById("objectives"), objectiveDeck);
-	displayDeck(document.getElementById("conditions"), conditionDeck);
+    // Displays the top three battle cards from each deck ready for selection
+    displayDeck(document.getElementById("deployments"), deploymentDeck);
+    displayDeck(document.getElementById("objectives"), objectiveDeck);
+    displayDeck(document.getElementById("conditions"), conditionDeck);
+}
+
+function displaySelectedBattleCards() {
+    
+    var battlefield = ["", "", ""];
+    
+    battlefield[0] = getLeftmostCard(deploymentDeck);
+    battlefield[1] = getLeftmostCard(objectiveDeck);
+    battlefield[2] = getLeftmostCard(conditionDeck);
+    
+    clearBattleCards();
+    displayDeck(document.getElementById("deployments"), battlefield);
+}
+
+function clearBattleCards() {
+    document.getElementById("deployments").innerHTML = "";
+    document.getElementById("objectives").innerHTML = "";
+    document.getElementById("conditions").innerHTML = "";
 }
 
 function passClicked() {
-	// Handles the pass button being clicked
-	changePlayer();
+    // Handles the pass button being clicked
+    changePlayer();
+    updateBattleStatus();
 }
 
 function showPassButton() {
-	document.getElementById("passButton").style.display = "block";
+    document.getElementById("passButton").style.display = "block";
+}
+
+function hidePassButton() {
+    document.getElementById("passButton").style.display = "none";
 }
 
 function setupCards() {
-	resetSetupStatus();
-	resetCardOpacity();
-	shuffleCards();
-	displayBattleCards();
-	showPassButton();
-	displayMessage(MSG_BLUE_TURN);
+    resetSetupStatus();
+    resetCardOpacity();
+    shuffleCards();
+    displayBattleCards();
+    showPassButton();
+    displayMessage(MSG_BLUE_TURN);
 }
 
 function init() {
-	generateDeckHTML(deploymentDeck);
-	generateDeckHTML(objectiveDeck);
-	generateDeckHTML(conditionDeck);
+    generateDeckHTML(deploymentDeck);
+    generateDeckHTML(objectiveDeck);
+    generateDeckHTML(conditionDeck);
 }
 
 // Initialize html for the cards
